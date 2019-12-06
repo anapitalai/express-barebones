c66d900e2cfbed31eece4cb182a67c9d27a459b4
Alois Napitalai <anapitalai@gmail.com>
2019-12-06 12:13:27 +1000

    Create app.js

---
+++ express-socketio/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const socket =require('socket.io');
+
+const app=express();
+
+const server = app.listen(2020,()=>{
+console.log('Server is listening on port 2020!');
+});
+
+
+//serve static html files from a directory called public
+app.use(express.static('public'));
+
+//setup socketio
+const io=socket(server);
+
+//when socket connection is established btw server and client
+io.on('connection',()=>{
+console.log('A connection was established');
+});
