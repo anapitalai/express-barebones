fa75f3fc23a5bc80f81a52fcf224e9171f82d2ca
Alois Napitalai <anapitalai@gmail.com>
2019-12-06 12:44:56 +1000

    Update socket.js

--- express-socketio/socket.js
+++ express-socketio/socket.js
@@ -1,2 +1,8 @@
 //establish socket connection
 const socket = io.connect('http://localhost:2020');
+
+
+//grab all the handles
+const taskHandle=document.getElementById='task-input';
+const displayHandle=document.getElementById='task-display';
+
