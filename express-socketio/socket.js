869a626a78611f3f12029fb6cbd5d6afc96728f1
Alois Napitalai <anapitalai@gmail.com>
2019-12-06 14:18:36 +1000

    Update socket.js

--- express-socketio/socket.js
+++ express-socketio/socket.js
@@ -3,12 +3,12 @@ const socket = io.connect('http://localhost:2020');
 
 
 //grab all the handles
-const taskHandle=document.getElementById='task-input';
-const displayHandle=document.getElementById='task-display';
-const btnHandle=document.getElementById='send';
+const taskHandle=document.getElementById('task-input');
+const displayHandle=document.getElementById('task-display');
+const btnHandle=document.getElementById=('send');
 
 //emit event to the server
-btnHandle.addEventListener('',('click')=>{
+btnHandle.addEventListener('click',()=>{
    
   //emits this message to the serve where the server emits to all connected clients
   socket..emit('task',{
@@ -19,7 +19,7 @@ btnHandle.addEventListener('',('click')=>{
 
 //listen for events emitted frm the server containing the data-taskdata
 socket.on('task',()=>{
-   displayHandle.innerHTML +=`<li>{{taskdata.task}}</li>`;
+   displayHandle.innerHTML +='<li>'+ taskdata.task + '</li>';
 });
 
 
